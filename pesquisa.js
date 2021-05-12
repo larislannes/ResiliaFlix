f7d480030b489c1d2df785e93859a7c6eeaa6022
andreeigenheer <eigenheer81@gmail.com>
2021-05-11 23:37:45 -0300

    buscador interno

---
+++ pesquisa.js
@@ -0,0 +1,17 @@
+function pesquisa() {
+    var busca, filtro, ul, li, a, i;
+    busca = document.getElementById("buscar");
+    filtro = busca.value.toUpperCase();
+    ul = document.getElementById("disponiveis");
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i< li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filtro) > -1) {
+            li[i].style.display = "";
+        }
+        else {
+            li[i].style.display = "none";
+        }
+    }
+}
